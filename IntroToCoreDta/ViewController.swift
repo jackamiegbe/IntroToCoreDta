1fce7796843e0879f69d6bd2d50f54bab60055c8
git@github.com:jackamiegbe/jackattack.git <geeksgofar@gmail.com>
2017-04-16 01:19:08 +0100

    progressing

--- IntroToCoreDta/ViewController.swift
+++ IntroToCoreDta/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Joseph", forKey: "username")
+        newUser.setValue("myPass", forKey: "password")
+        newUser.setValue(35, forKey: "age")
+        
+        do {
+            try context.save()
+            print("Saved")
+        } catch {
+            print("There was an error")
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let username = result.value(forKey: "username") as? String {
+                        print(username)
+                    }
+                }
+            } else {
+                print("No results")
+            }
+        } catch {
+            print("Couldn't fetch results")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
